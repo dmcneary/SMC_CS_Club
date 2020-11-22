65e9b50aea84ee53a45c7a343d9e6ac32d0ab0c1
David McNeary <djmcwebdev@gmail.com>
2020-11-21 17:34:05 -0800

    var to const

--- leetcode_solns/solution-13.js
+++ leetcode_solns/solution-13.js
@@ -52,29 +52,29 @@ Constraints:
 s contains only the characters ('I', 'V', 'X', 'L', 'C', 'D', 'M').
 It is guaranteed that s is a valid roman numeral in the range [1, 3999].
 */
-/**
- * @param {string} s
- * @return {number}
- */
-var romanToInt = function(s) {
-    let map = {
-        "I" : 1,
-        "V" : 5,
-        "X" : 10,
-        "L" : 50,
-        "C" : 100,
-        "D" : 500,
-        "M" : 1000
-    };
-    let num = 0;
-    
-    for (let i = 0; i < s.length; i++) {
-        if (map[s[i]] < map[s[i + 1]]) {
-            num += map[s[i + 1]] - map[s[i]];
-            i++;
-        } else {
-            num += map[s[i]];
+    /**
+     * @param {string} s
+     * @return {number}
+     */
+    const romanToInt = function(s) {
+        let map = {
+            "I" : 1,
+            "V" : 5,
+            "X" : 10,
+            "L" : 50,
+            "C" : 100,
+            "D" : 500,
+            "M" : 1000
+        };
+        let num = 0;
+        
+        for (let i = 0; i < s.length; i++) {
+            if (map[s[i]] < map[s[i + 1]]) {
+                num += map[s[i + 1]] - map[s[i]];
+                i++;
+            } else {
+                num += map[s[i]];
+            }
         }
-    }
-    return num;
-};
+        return num;
+    };
